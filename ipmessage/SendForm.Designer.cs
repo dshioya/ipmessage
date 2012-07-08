86f938e0b7f2b8512c666c79e65c55e977dc618e
shioya <d.shioya@wiz-net.jp>
2012-07-08 16:49:11 +0900

    送信フォームのUI部分をおおまかに実装

--- ipmessage/SendForm.Designer.cs
+++ ipmessage/SendForm.Designer.cs
@@ -28,20 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.accountList = new System.Windows.Forms.ListBox();
+            this.messageText = new System.Windows.Forms.TextBox();
+            this.messageLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // accountList
+            // 
+            this.accountList.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.accountList.FormattingEnabled = true;
+            this.accountList.ItemHeight = 16;
+            this.accountList.Location = new System.Drawing.Point(13, 13);
+            this.accountList.Name = "accountList";
+            this.accountList.Size = new System.Drawing.Size(267, 116);
+            this.accountList.TabIndex = 0;
+            // 
+            // messageText
+            // 
+            this.messageText.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.messageText.Location = new System.Drawing.Point(13, 167);
+            this.messageText.Multiline = true;
+            this.messageText.Name = "messageText";
+            this.messageText.Size = new System.Drawing.Size(267, 46);
+            this.messageText.TabIndex = 1;
+            this.messageText.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MessageText_KeyDown);
+            this.messageText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MessageText_KeyPress);
+            // 
+            // messageLabel
+            // 
+            this.messageLabel.AutoSize = true;
+            this.messageLabel.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.messageLabel.Location = new System.Drawing.Point(13, 149);
+            this.messageLabel.Name = "messageLabel";
+            this.messageLabel.Size = new System.Drawing.Size(62, 15);
+            this.messageLabel.TabIndex = 2;
+            this.messageLabel.Text = "メッセージ";
             // 
             // SendForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(292, 237);
+            this.Controls.Add(this.messageLabel);
+            this.Controls.Add(this.messageText);
+            this.Controls.Add(this.accountList);
             this.Name = "SendForm";
-            this.Text = "Form1";
+            this.Text = "送信";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox accountList;
+        private System.Windows.Forms.TextBox messageText;
+        private System.Windows.Forms.Label messageLabel;
     }
 }
 
--- ipmessage/SendForm.cs
+++ ipmessage/SendForm.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Collections;
 
 namespace ipmessage
 {
@@ -14,9 +16,111 @@ namespace ipmessage
      */
     public partial class SendForm : Form
     {
-        public SendForm()
+        /**
+         * アカウントCSVファイル名。
+         */
+        private string accountCsvFilename = "account.csv";
+
+        /**
+         * アカウント情報のキャッシュ一覧。
+         */
+        private Hashtable accountHash = new Hashtable();
+        private Hashtable ipHash = new Hashtable();
+
+        /**
+         * コンストラクタ。
+         */
+        public SendForm(string header = "")
         {
             InitializeComponent();
+
+            // アカウント情報をロードする
+            if (!readAccountCsv())
+            {
+                // ロードに失敗した場合はアプリケーション終了
+                Application.Exit();
+            }
+            else
+            {
+                // アカウントリストボックスを更新する
+                foreach (string name in accountHash.Keys)
+                {
+                    accountList.Items.Add(name);
+                }
+            }
+
+            if (header.Length > 0)
+            {
+                // 受信メッセージをテキストボックスに設定する
+                messageText.Text = "> " + header + "\n";
+            }
+
         }
+
+        /**
+         * アカウントCSVファイルをロードする。
+         */
+        private bool readAccountCsv()
+        {
+            if (!File.Exists(accountCsvFilename))
+            {
+                // ファイルが存在しない場合
+                return false;
+            }
+
+            // CSVファイルを読込んでアカウントリストに表示する
+            StreamReader sr = new StreamReader(accountCsvFilename, Encoding.GetEncoding("Shift_JIS"));
+
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] data = line.Split(',');
+                if (data.Length > 1)
+                {
+                    accountHash.Add(data[0], data[1]);
+                    ipHash.Add(data[1], data[0]);
+                }
+            }
+
+            // ストリームクローズ
+            sr.Close();
+
+            return true;
+        }
+
+        /**
+         * Enterキーのみ押されたかどうかのフラグ。
+         */
+        private bool isPressedEnterKey;
+
+        /**
+         * メッセージボックスのkeydownイベントハンドラ。
+         */
+        private void MessageText_KeyDown(object sender, KeyEventArgs e)
+        {
+            isPressedEnterKey = false;
+
+            if (e.KeyData == Keys.Enter && e.Modifiers != Keys.Shift)
+            {
+                isPressedEnterKey = true;
+            }
+        }
+
+        /**
+         * メッセージボックスのkeypressイベントハンドラ。
+         */
+        private void MessageText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (isPressedEnterKey)
+            {
+                // キーハンドリングはキャンセルする
+                e.Handled = true;
+
+                // TODO:メッセージを送信する
+                MessageBox.Show(messageText.Text);
+            }
+        }
+
     }
+
 }
